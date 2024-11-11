d8a9caee63d37757b2d16baa8a66385ded779641
s0sococo <sstandsforstarlet@gmail.com>
2024-11-11 14:16:08 +0100

    -

---
+++ backend-rental/AppRental/Migrations/20241111131505_InitialCreate2.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AppRental.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Cost",
+                table: "Cars",
+                newName: "BaseCostPerDay");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "BaseCostPerDay",
+                table: "Cars",
+                newName: "Cost");
+        }
+    }
+}
