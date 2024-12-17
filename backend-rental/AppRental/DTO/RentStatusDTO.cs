d1c0ee6a0d28d62ef38632da42d270524c4245b5
kTrzcinskii <kt.trzcinski@gmail.com>
2024-12-17 15:14:58 +0100

    Return end date with rent status to update data on browser

--- backend-rental/AppRental/DTO/RentStatusDTO.cs
+++ backend-rental/AppRental/DTO/RentStatusDTO.cs
@@ -5,12 +5,14 @@ namespace AppRental.DTO;
 public class RentStatusDTO
 {
     public required string Status { get; set; }
+    public DateTime? EndDate { get; set; }
 
     public static RentStatusDTO FromRent(Rent rent)
     {
         return new RentStatusDTO
         {
-            Status = rent.Status.ToString()
+            Status = rent.Status.ToString(),
+            EndDate = rent.EndDate
         };
     }
 }
